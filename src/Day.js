065be2ee69524ba818c9474c42ae48aa027e3445
James Narayanan <jamesnarayanan@gmail.com>
2019-01-29 21:53:46 -0500

    Removed useless constructor

--- src/Day.js
+++ src/Day.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './Day.css';
 
 class Day extends Component {
-	constructor(props) {
-		super(props);
-	}
 	render() {
 		return (
 			<div id="dayWrapper">
