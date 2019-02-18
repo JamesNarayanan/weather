5f5a7ce0fffc2d8725e4b5539d5c3f205017fd66
James Narayanan <jamesnarayanan@gmail.com>
2019-02-18 18:05:29 -0500

    Only allows the user to enter valid degree values
    Values above 180 will be rounded down to 180 (same with negatives)

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
 		this.handleDayClick = this.handleDayClick.bind(this);
 		this.getLocation = this.getLocation.bind(this);
 		this.getCurrentLocation = this.getCurrentLocation.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
 	}
 
 	handleDayClick(dayNumber) {
@@ -32,25 +33,31 @@ class App extends Component {
 	}
 
 	getCurrentLocation() {
+		this.setState({haveLocation: true});
 		navigator.geolocation.getCurrentPosition(position => {
 			this.setState({lat: position.coords.latitude, lon: position.coords.longitude});
 			this.getLocation();
 		});
 	}
 
+	handleKeyPress(event) {
+		if(event.key === "Enter")
+			this.getLocation();
+	}
+
 	render() {
 		if(!this.state.haveLocation) {
 			return (
 				<div id="getLocWrapper" className="wrapper">
-					<div onClick={this.getCurrentLocation} className="button">Get Current Location</div>
+					<button onClick={this.getCurrentLocation}>Get Current Location</button>
 					<div style={{margin: "20px"}}>Or</div>
 					<div id="otherLoc">
 						<div>Enter a location</div>
-						<div>
-							<input placeholder="Latitude" type="number" value={this.state.lat} onChange={event => this.setState({lat: event.target.value})}></input>
-							<input placeholder="Longitude" type="number" value={this.state.lon} onChange={event => this.setState({lon: event.target.value})}></input>
+						<div id="inputWrapper">
+							<input placeholder="Latitude" type="number" value={this.state.lat}onChange={event => Math.abs(event.target.value) > 180 ? this.setState({lat: event.target.value/Math.abs(event.target.value)*180}) : this.setState({lat: event.target.value})} onKeyPress={this.handleKeyPress}></input>
+							<input placeholder="Longitude" type="number" value={this.state.lon} onChange={event => Math.abs(event.target.value) > 180 ? this.setState({lon: event.target.value/Math.abs(event.target.value)*180}) : this.setState({lon: event.target.value})} onKeyPress={this.handleKeyPress}></input>
 						</div>
-						<div onClick={this.getLocation} className="button" style={{marginTop: "10px"}}>Submit</div>
+						<button onClick={this.getLocation} style={{marginTop: "10px"}}>Submit</button>
 					</div>
 				</div>
 			);
