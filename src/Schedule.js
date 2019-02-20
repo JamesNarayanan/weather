cc801c2c42f31eef175507a3c8875e1aab3edff8
James Narayanan <jamesnarayanan@gmail.com>
2019-02-20 18:13:22 -0500

    Now makes day objects with loop
    Used to make seven day objects
    Now loops through this.props.days

--- src/Schedule.js
+++ src/Schedule.js
@@ -4,17 +4,16 @@ import Day from './Day';
 
 class Schedule extends Component {
 	render() {
+		var days = [];
+		for(var i = 0; i<this.props.days.length; i++){
+			days.push(<Day dayNum={i} days={this.props.days} selectedDay={this.props.selectedDay} onClick={this.props.onDayClick} unit={this.props.unit}/>);
+		};
+		
 		return (
 			<div id="scheduleWrapper">
-				<Day dayNum={0} days={this.props.days} selectedDay={this.props.selectedDay} onClick={this.props.onDayClick} unit={this.props.unit}/>
-				<Day dayNum={1} days={this.props.days} selectedDay={this.props.selectedDay} onClick={this.props.onDayClick} unit={this.props.unit}/>
-				<Day dayNum={2} days={this.props.days} selectedDay={this.props.selectedDay} onClick={this.props.onDayClick} unit={this.props.unit}/>
-				<Day dayNum={3} days={this.props.days} selectedDay={this.props.selectedDay} onClick={this.props.onDayClick} unit={this.props.unit}/>
-				<Day dayNum={4} days={this.props.days} selectedDay={this.props.selectedDay} onClick={this.props.onDayClick} unit={this.props.unit}/>
-				<Day dayNum={5} days={this.props.days} selectedDay={this.props.selectedDay} onClick={this.props.onDayClick} unit={this.props.unit}/>
-				<Day dayNum={6} days={this.props.days} selectedDay={this.props.selectedDay} onClick={this.props.onDayClick} unit={this.props.unit}/>
-				<Day dayNum={7} days={this.props.days} selectedDay={this.props.selectedDay} onClick={this.props.onDayClick} unit={this.props.unit}/>
-			</div>);
+				{days}
+			</div>
+		);
 	}
 }
 
