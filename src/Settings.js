87c4d58c8feb86fc93940b4a2ac8edefc9b47187
James Narayanan <jamesnarayanan@gmail.com>
2019-05-06 14:03:28 -0400

    Removed commented code from Settings and unnecessary css file

--- src/Settings.js
+++ src/Settings.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import './Settings.css';
 
 class Settings extends Component {
     render() {
         return (
-            <div /* id="settingsWrapper" */ className="mx-auto col-12 col-sm-6 col-md-3 col-lg-2 p-3 border rounded border-dark">
-                {/* <div style={{display: "flex", flexDirection: "row"}}> */}
+            <div className="mx-auto col-12 col-sm-6 col-md-3 col-lg-2 p-3 border rounded border-dark">
                 <div className="row d-flex justify-content-center">
-                    <button className="col-4 btn" /* id="F" */ onClick={()=>this.props.onTempClick("F")} style={{border: this.props.unit === "im" ? "1px solid black" : "none"}}>F°</button>
-                    <button className="col-4 btn" /* id="C" */ onClick={()=>this.props.onTempClick("C")} style={{border: this.props.unit === "si" ? "1px solid black" : "none"}}>C°</button>
+                    <button className="col-4 btn" onClick={()=>this.props.onTempClick("F")} style={{border: this.props.unit === "im" ? "1px solid black" : "none"}}>F°</button>
+                    <button className="col-4 btn" onClick={()=>this.props.onTempClick("C")} style={{border: this.props.unit === "si" ? "1px solid black" : "none"}}>C°</button>
                 </div>
             </div>
         );
