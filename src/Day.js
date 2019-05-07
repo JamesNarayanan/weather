9472e46001f9495e98075fe0f96d962803cf9896
James Narayanan <jamesnarayanan@gmail.com>
2019-05-06 21:10:42 -0400

    Updated DayInfo to use bootstrap
    Is now very responsive and mobile optomized
    
    Also removed uncessary DayInfo.css

--- src/Day.js
+++ src/Day.js
@@ -31,9 +31,8 @@ class Day extends Component {
 		return (
 			<div id="dayWrapper" className="m-2 px-0 py-2 d-flex flex-column align-items-center rounded" onClick={()=>this.props.onClick(this.props.dayNum)} style={{backgroundColor: this.props.selectedDay === this.props.dayNum ? "rgba(0,0,0,.2)" : "", width: "100px"}}>
 				<div className="h5 mb-0">{getWeekday(day.time)}</div>
-				<br/>
-				<Skycons className="w-100 h-auto my-0" color="white" icon={iconName} autoplay={true}/>
-				<div className="mt-2 mb-0 d-flex h6">
+				<Skycons className="w-100 h-auto my-2" color="white" icon={iconName} autoplay={true}/>
+				<div className="my-0 d-flex h6">
 					<div>{Math.round(high)}°</div> &nbsp;&nbsp; <div className="text-muted">{Math.round(low)}°</div>
 				</div>
 			</div>
--- src/DayInfo.js
+++ src/DayInfo.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
-import './DayInfo.css';
 
 class DayInfo extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { width: 0, height: 0 };
+		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+	}
+	componentDidMount() {
+		this.updateWindowDimensions();
+		window.addEventListener('resize', this.updateWindowDimensions);
+	}
+	
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateWindowDimensions);
+	}
+	
+	updateWindowDimensions() {
+		this.setState({width: window.innerWidth});
+	}
 	render() {
 		function getWeekday(UNIX_timestamp){
 			var date = new Date(UNIX_timestamp * 1000);
@@ -26,16 +42,20 @@ class DayInfo extends Component {
 			precipAccum *= 2.54;
 			measurementUnit = "cm";
 		}
-		
+		var textSizing = "h1";
+		if(this.state.width <= 767) {
+			textSizing = this.state.width <= 575 ? "h4" : "h3";
+		}
+
 		return (
-			<div id="dayInfoWrapper">
-				<div id="left">
-					<div id="dow">{getWeekday(this.props.day.time)}</div>
-					<div id="precipType">{this.props.day.precipProb >= .3 ? this.props.day.precipType : ""}</div>
-					<div id="temp">{Math.round(temp)}°</div>
-					<div id="summary">{this.props.day.summary}</div>
+			<div className="row mx-2 mb-3">
+				<div className="col-12 col-md-6 mb-3 mb-md-0 d-flex flex-column text-center text-md-left">
+					<div className="display-4">{getWeekday(this.props.day.time)}</div>
+					<div className="h3 text-capitalize">{this.props.day.precipProb >= .3 ? this.props.day.precipType : ""}</div>
+					<div className="display-1 mt-n3 mb-n2">{Math.round(temp)}°</div>
+					<div>{this.props.day.summary}</div>
 				</div>
-				<div id="right">
+				<div className={`col-12 col-md-6 ${textSizing} font-weight-normal d-flex flex-column text-center text-md-left`}>
 					<div>Precipitation: {Math.round(this.props.day.precipProb*100)}%</div>
 					{precipAccum && this.props.day.precipType === "snow" && this.props.day.precipProb >= .3 ? <div>Accumulation: {`${precipAccum.toFixed(2)} ${measurementUnit}`}</div> : ""}
 					<div>Humidity: {Math.round(this.props.day.humidity*100)}%</div>
